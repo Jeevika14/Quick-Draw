a79b2d9403750962298cdb9ccbecc28e96d3db29
Jeevika14 <91791370+Jeevika14@users.noreply.github.com>
2022-12-30 15:05:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+quick_draw_data_set=["aircraft carrier","airplane","alarm clock","ambulance","angel","animal migration","ant","anvil","apple","arm","asparagus","axe","backpack","banana","bandage","barn","baseball","baseball bat","basket","basketball","bat","bathtub","beach","bear","beard","bed","bee","belt","bench","bicycle","binoculars","bird","birthday cake","blackberry","blueberry","book","boomerang","bottlecap","bowtie","bracelet","brain","bread","bridge","broccoli","broom","bucket","bulldozer","bus","bush","butterfly","cactus","cake","calculator","calendar","camel","camera","camouflage","campfire","candle","cannon","canoe","car","carrot","castle","cat","ceiling fan","cello","cell phone","chair","chandelier","church","circle","clarinet","clock","cloud","coffee cup","compass","computer","cookie","cooler","couch","cow","crab","crayon","crocodile","crown","cruise ship","cup","diamond","dishwasher","diving board","dog","dolphin","donut","door","dragon","dresser","drill","drums","duck","dumbbell","ear", "elbow","elephant","envelope","eraser","eye"];
+
+random_number=  Math.floor((Math.random()*quick_draw_data_set.length)+1);
+console.log(quick_draw_data_set[random_number]);
+sketch= quick_draw_data_set[random_number];
+document.getElementById('sketch_name').innerHTML= 'Sketch to be drawn: ' + sketch;
+timer_counter= 0;
+timer_check= "";
+drawn_sketch= "";
+answer_holder= "";
+score= 0;
